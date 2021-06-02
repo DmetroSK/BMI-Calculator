3b25a47f550361d15751fb24858d519b6deaa254
Pramuditha Malhara <ygpramuditha@gmail.com>
2021-06-03 03:25:56 +0530

    BMI logic

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +14,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText age;
-    private EditText height;
-    private EditText weight;
-
-
+    String error,category;
+    Float result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +24,125 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button btn_calculate_button = (Button) findViewById(R.id.btn_calculate);
+        EditText age = (EditText) findViewById(R.id.txt_age);
+        EditText height = (EditText) findViewById(R.id.txt_height);
+        EditText weight = (EditText) findViewById(R.id.txt_weight);
 
         btn_calculate_button.setOnClickListener(new View.OnClickListener() {
-
-
-
+            @Override
             public void onClick(View v) {
-                System.out.println("Button Clicked");
 
-                Intent activity2Intent = new Intent(getApplicationContext(), SecondActivity.class);
-                startActivity(activity2Intent);
+                SetValidation();
+
+                Float height_value = Float.parseFloat(height.getText().toString());
+                Float weight_value = Float.parseFloat(weight.getText().toString());
+
+                if(Integer.parseInt(age.getText().toString()) < 12)
+                {
+                    Toast.makeText(getApplicationContext(),"Enter 18+ Age", Toast.LENGTH_SHORT).show();
+                }
+
+                else
+                {
+                    Float meter = height_value/100;
+
+                    Float result = weight_value/(meter*meter);
+
+                //    SetCategory();
+
+                  Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+
+
+//
+//                Intent activity2Intent = new Intent(getApplicationContext(), SecondActivity.class);
+//                startActivity(activity2Intent);
+//
+//                Intent sendIntent = new Intent();
+//                sendIntent.setAction(Intent.ACTION_SEND);
+//                sendIntent.putExtra(Intent.EXTRA_SUBJECT,"Message Test");
+//                sendIntent.setType("text/plain");
+//
+//                startActivity(sendIntent);
             }
+
+            public void SetCategory(){
+
+                if(result < 16)
+                {
+                    category = "Severe Thinness";
+                }
+                else if(16 <= result || result < 17)
+                {
+                    category = "Moderate Thinness";
+                }
+                else if(17 <= result || result < 18.5)
+                {
+                    category = "Mild Thinness";
+                }
+                else if(18.5 <= result || result < 25)
+                {
+                    category = "Normal";
+                }
+                else if(25 <= result || result < 30)
+                {
+                    category = "Overweight";
+                }
+                else if(30 <= result || result < 35)
+                {
+                    category = "Obese Class I";
+                }
+                else if(35 <= result || result < 40)
+                {
+                    category = "Obese Class II";
+                }
+                else
+                {
+                    category = "Obese Class III";
+                }
+
+                Toast.makeText(getApplicationContext(), category.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            public void SetValidation() {
+
+                if (age.getText().toString().isEmpty()) {
+                    error = "Enter Age";
+                    if (height.getText().toString().isEmpty()) {
+                         if (weight.getText().toString().isEmpty()) {
+                            error = "Enter Values";
+                        }
+                         else
+                         {
+                             error = "Enter Age and Height";
+                         }
+                    }
+                    else if(weight.getText().toString().isEmpty())
+                    {
+                        error = "Enter Age and Weight";
+                    }
+                    else
+                    {
+                        error = "Age";
+                    }
+                  }
+                else if(height.getText().toString().isEmpty())
+                {
+                    error = "Enter Height";
+                }
+                else
+                {
+                    error = "Enter Weight";
+                }
+
+                Toast.makeText(getApplicationContext(), error, Toast.LENGTH_SHORT).show();
+            }
+
+
 
 
         });
--- app/src/main/java/com/example/bmicalculator/SecondActivity.java
+++ app/src/main/java/com/example/bmicalculator/SecondActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,7 +14,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        Toast.makeText(getApplicationContext(),extras.getString(Intent.EXTRA_SUBJECT,null),Toast.LENGTH_LONG).show();
     }
 
-    
+
 }
